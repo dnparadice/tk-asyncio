bdb85f801d044a532ad0ebc89d306d7bfaa787cd
Paradice City <dnparadice@mac.com>
2023-03-25 12:40:20 -0600

    added init and setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AsyncTk',
+    version='0.0.1',
+    packages=['tkasyncio'],
+    url='https://github.com/dnparadice/tk-asyncio',
+    license='MIT',
+    author='paradicecity',
+    author_email='dontemailme@lol.com',
+    description='Tkinter wiht AsyncIO'
+)
