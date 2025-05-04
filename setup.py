e6bf9f0af8ff7d11d2ede30afa329b48a09c6993
paradicecity <jaysonbcollins@gmail.com>
2025-05-04 09:28:05 -0600

    updated repo name to tkasyncio so that it can be imported directly

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='AsyncTk',
     version='0.0.1',
     packages=['tkasyncio'],
-    url='https://github.com/dnparadice/tk-asyncio',
+    url='https://github.com/dnparadice/tkasyncio',
     license='MIT',
     author='paradicecity',
     author_email='dontemailme@lol.com',
